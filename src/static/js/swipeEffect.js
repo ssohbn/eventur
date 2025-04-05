d004713a4be5d56b481c536d979387505b620c06
RussianGravy <valentinejones1215@gmail.com>
2025-04-05 10:09:01 -0700

    added swipe trigger to check and x buttons

--- src/static/js/swipeEffect.js
+++ src/static/js/swipeEffect.js
@@ -1,8 +1,16 @@
-const swipeLeft = (id) => {
+var id = "Event 2";
+
+const swipeLeft = () => {
   const card = document.getElementById(id);
   card.classList.add("swipe-left-fade");
+  id = "Event 1";
 };
-const swipeRight = (id) => {
+const swipeRight = () => {
   const card = document.getElementById(id);
   card.classList.add("swipe-right-fade");
+  id = "Event 1";
 };
+
+const updateBackgound = () => {
+    
+}
