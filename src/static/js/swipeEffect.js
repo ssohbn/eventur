6752685d545871dec431d9629255866778345918
RussianGravy <valentinejones1215@gmail.com>
2025-04-05 12:21:15 -0700

    fixed navbar

--- src/static/js/swipeEffect.js
+++ src/static/js/swipeEffect.js
@@ -1,16 +1,32 @@
-var id = "Event 2";
+var cards = [
+  {
+    Title: "Event 2",
+    Blurb: "This is the second event.",
+    img: "https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg",
+  },
+  {
+    Title: "Event 1",
+    Blurb: "This is the first event.",
+    img: "https://as2.ftcdn.net/v2/jpg/04/96/15/83/1000_F_496158338_SgDd7OQQC2QVfN7U5Qijl2muktM0LjjG.jpg",
+  },
+];
+var i = 0;
+
+const updateBackgound = (img) => {
+  document.body.style.backgroundImage = `url(${img})`;
+};
+updateBackgound(cards[i].img);
 
 const swipeLeft = () => {
-  const card = document.getElementById(id);
+  const card = document.getElementById(cards[i].Title);
   card.classList.add("swipe-left-fade");
-  id = "Event 1";
-};
-const swipeRight = () => {
-  const card = document.getElementById(id);
-  card.classList.add("swipe-right-fade");
-  id = "Event 1";
+  i += 1;
+  updateBackgound(cards[i].img);
 };
 
-const updateBackgound = () => {
-    
-}
+const swipeRight = () => {
+  const card = document.getElementById(cards[i].Title);
+  card.classList.add("swipe-right-fade");
+  i += 1;
+  updateBackgound(cards[i].img);
+};
