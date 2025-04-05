6438a4cbc2ade3badb11dac3fc6c569defcdf742
RussianGravy <valentinejones1215@gmail.com>
2025-04-04 17:54:33 -0700

    removed templates, added static file routes

--- src/main.go
+++ src/main.go
@@ -4,29 +4,21 @@ import (
 	"net/http"
 
   "github.com/gin-gonic/gin"
-
-  "html/template"
 )
 
 func main() {
   r := gin.Default()
+
+  //routes
+	r.StaticFile("/", "src/static/index.html")
+  r.StaticFile("/create", "src/static/create.html")
+  r.StaticFile("/profile", "src/static/profile.html")
+  r.StaticFile("/events", "src/static/events.html")
+
   r.GET("/ping", func(c *gin.Context) {
     c.JSON(http.StatusOK, gin.H{
       "message": "pong",
     })
-  })
-  //front end routes
-  templ := template.Must(template.ParseFiles(
-    "src/templates/layout.html",
-    "src/templates/index.html",
-    "src/templates/partials/navbar.html", 
-  ))
-  r.SetHTMLTemplate(templ)
-
-  r.GET("/", func(c *gin.Context) {
-      c.HTML(http.StatusOK, "layout.html", gin.H{
-          "Title": "Home Page",
-      })
   })
   // run the server
   r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
