b547f2cd0fb00899468666c3b5c2c0b06a002ee7
RussianGravy <valentinejones1215@gmail.com>
2025-04-05 15:26:52 -0700

    added pop up effect

--- src/static/js/swipeEffect.js
+++ src/static/js/swipeEffect.js
@@ -9,6 +9,11 @@ var cards = [
     Blurb: "This is the second event.",
     img: "https://as2.ftcdn.net/v2/jpg/04/96/15/83/1000_F_496158338_SgDd7OQQC2QVfN7U5Qijl2muktM0LjjG.jpg",
   },
+  {
+    Title: "Midnight Concert",
+    Blurb: "This is the first event.",
+    img: "",
+  },
 ];
 var i = 0;
 
@@ -23,6 +28,7 @@ const swipeLeft = () => {
   card.classList.add("swipe-left-fade");
   i += 1;
   updateBackgound(cards[i].img);
+  fadeInOut("Not interested!", "#f56e64");
 };
 
 const swipeRight = () => {
@@ -31,4 +37,20 @@ const swipeRight = () => {
   card.classList.add("swipe-right-fade");
   i += 1;
   updateBackgound(cards[i].img);
+  fadeInOut("Sounds Fun!", "#73f564");
+};
+
+const fadeInOut = (message, color) => {
+  const popup = document.getElementById("pop-up");
+  popup.innerHTML = message;
+  popup.style.backgroundColor = color;
+  popup.classList.add("fade-in");
+  const delay = 500; // 1/2 seconds
+  setTimeout(() => {
+    popup.classList.remove("fade-in");
+    popup.classList.add("fade-out");
+  }, delay);
+  setTimeout(() => {
+    popup.classList.remove("fade-out");
+  }, delay * 2);
 };
